35d27380b32b69ea1838c658e7105a80982d4c25
Refo Ilmiya <refo.ilmiya@gmail.com>
2023-02-24 14:08:52 +0100

    Finishing the code added documentation

--- view_from_point_cloud.py
+++ view_from_point_cloud.py
@@ -1,6 +1,6 @@
 import open3d as o3d
 
 if __name__ == "__main__":
-    file_name = "speaker2.ply_1677242216965.86499023437500.ply"
+    file_name = "speaker5.ply_1677243611687.82519531250000.ply"
     pcd = o3d.io.read_point_cloud(file_name)
     o3d.visualization.draw(pcd)
